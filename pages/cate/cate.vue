55a6124d795cc9974df637558d7c5c9f9f5045fe
愿得一心人 <9643735+Amber1117@user.noreply.gitee.com>
2022-11-21 15:45:01 +0800

    完成分类页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,26 @@
 <template>
-  <view>
-    cate
+  <view class="scroll-view-container">
+    <!-- 左侧滚动部分 -->
+    <scroll-view scroll-y class="left-scroll-view" :style="{height: wh + 'px'}">
+      <block v-for="(item, i) in cateList" :key="i">
+        <view :class="['left-scroll-view-item', i === active?'active': '']" @click="activeChanged(i)">{{item.cat_name}}
+        </view>
+      </block>
+    </scroll-view>
+    <!-- 右侧滚动部分 -->
+    <scroll-view scroll-y class="right-scroll-view" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+      <view class="cate-lv2" v-for="(item2,i2) in cateLevel2" :key="i2">
+        <!-- 二级分类标题 -->
+        <view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+        <!-- 三级分类列表 -->
+        <view class="cate-lv3-list">
+          <view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+            <image :src="item3.cat_icon"></image>
+            <text>{{item3.cat_name}}</text>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
   </view>
 </template>
 
@@ -8,12 +28,107 @@
   export default {
     data() {
       return {
-        
+        wh, //可用高度
+        cateList: [], //分类数据
+        active: 0, //一级分类选中项
+        cateLevel2: [], //二级分类数据
+        scrollTop: 0 // 滚动条距离顶部的距离
       };
+    },
+    onLoad() {
+      //获取设备信息
+      const sysInfo = uni.getSystemInfoSync()
+      this.wh = sysInfo.windowHeight,
+        this.getCateList()
+    },
+    methods: {
+      //获取分类数据
+      async getCateList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/categories')
+        if (res.meta.status !== 200) return uni.$showMsg('数据请求失败!')
+        this.cateList = res.message
+        this.cateLevel2 = res.message[0].children
+      },
+      //切换分类
+      activeChanged(i) {
+        this.active = i
+        //重新为两级分类赋值
+        this.cateLevel2 = this.cateList[i].children,
+        //重置滚动条位置 让 scrollTop 的值在 0 与 1 之间切换
+          this.scrollTop = this.scrollTop === 0 ? 1 : 0
+      },
+      gotoGoodsList(item3) {
+        uni.navigateTo({
+          url:'/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .scroll-view-container {
+    display: flex;
 
+    .left-scroll-view {
+      width: 120px;
+
+      .left-scroll-view-item {
+        line-height: 60px;
+        text-align: center;
+        background-color: #f7f7f7;
+        font-size: 12px;
+
+        &.active {
+          background-color: white;
+          position: relative;
+
+          &::before {
+            content: '';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            top: 50%;
+            left: 0%;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+
+    .cate-lv3-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .cate-lv3-item {
+        display: flex;
+        margin-bottom: 10px;
+        ;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 33.33%;
+
+        image {
+          width: 60px;
+          height: 60px;
+        }
+
+        text {
+          font-size: 12px;
+        }
+      }
+    }
+  }
 </style>
